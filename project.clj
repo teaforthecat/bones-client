b66e9370a803b48e21fa2494b334c2ac4c6734ac
Chris Thopmson <teaforthecat@gmail.com>
2017-02-28 20:08:39 -0600

    bump 0.2.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bones/client "0.2.3"
+(defproject bones/client "0.2.4"
   :description "cljs client for bones/http"
   :url "https://github.com/teaforthecat/bones-client"
   :license {:name "Eclipse Public License"
